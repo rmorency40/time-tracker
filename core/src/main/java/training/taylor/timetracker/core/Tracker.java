a417f0505b016c3f054272a58bb9c7dc3ac64ba6
rmorency40 <30129346+rmorency40@users.noreply.github.com>
2019-07-09 12:17:33 -0500

    Revert back all the  changes

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,9 +16,9 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true) {
+        if (true)
             entries.remove(entry);
-        }
+
         entries.remove(entry);
     }
 
@@ -27,7 +27,16 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
+        try {
 
+        } catch (Exception e) {
+
+        }
+
+        boolean valid = false;
+
+        if (valid = true) {
+            // whatever
         }
 
         return entries.get(index);
